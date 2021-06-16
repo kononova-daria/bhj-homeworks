e1fc6105e5333267e913bda4a7643387cdda6dd8
Kononova Daria <kononova.yu.daria@mail.ru>
2021-06-16 22:08:46 +0300

    Решение 2.2 №2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,15 +3,19 @@ let allMenuSub = document.getElementsByClassName("menu_sub");
 
 for (let i = 0; i < links.length; i++) {
 	links.item(i).onclick = function () {
-		for (let i = 0; i < allMenuSub.length; i++) {
-			allMenuSub.item(i).classList.remove("menu_active");
-		}
 		let menu = links.item(i).closest("li");
 		let menuSub = menu.querySelector("ul.menu_sub");
 		if (menuSub !== null) {
-			menuSub.classList.add("menu_active");
-			return false;
+			if (menuSub.classList.contains("menu_active")) {
+				menuSub.classList.remove("menu_active");
+				return false;
+			} else {
+				for (let i = 0; i < allMenuSub.length; i++) {
+					allMenuSub.item(i).classList.remove("menu_active");
+				}
+				menuSub.classList.add("menu_active");
+				return false;
+			}	
 		}
 	}
 }
-
