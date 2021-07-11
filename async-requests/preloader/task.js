b9fd0341e46637f25489248ce4d8ffa5156f7820
Kononova Daria <kononova.yu.daria@mail.ru>
2021-07-11 20:17:20 +0300

    Решение 7.1

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,19 @@
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com', false);
+xhr.send();
+
+if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+	const image = document.getElementById('loader');
+	image.classList.remove('loader_active');
+
+	const items = document.getElementById('items');
+	const data = JSON.parse(xhr.response);
+
+	for (let key in data.response.Valute) {
+		items.insertAdjacentHTML('beforeEnd', '<div class="item"><div class="item__code">USD</div><div class="item__value">32</div><div class="item__currency">руб.</div></div>');
+
+		items.lastChild.querySelector('.item__code').textContent = data.response.Valute[key].CharCode;
+		items.lastChild.querySelector('.item__value').textContent = data.response.Valute[key].Value;
+	}
+}
