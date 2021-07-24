5dc6bf44cbd8fc941f9c83d1bb8a48236a24e69f
Kononova Daria <kononova.yu.daria@mail.ru>
2021-07-24 15:47:21 +0300

    Решение 7.1 №2

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,19 +1,22 @@
 let xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com', false);
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
 xhr.send();
 
-if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-	const image = document.getElementById('loader');
-	image.classList.remove('loader_active');
+xhr.onreadystatechange = function () {
+	if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+		const image = document.getElementById('loader');
+		image.classList.remove('loader_active');
 
-	const items = document.getElementById('items');
-	const data = JSON.parse(xhr.response);
+		const items = document.getElementById('items');
+		const data = JSON.parse(xhr.response);
 
-	for (let key in data.response.Valute) {
-		items.insertAdjacentHTML('beforeEnd', '<div class="item"><div class="item__code">USD</div><div class="item__value">32</div><div class="item__currency">руб.</div></div>');
+		for (let key in data.response.Valute) {
+			items.insertAdjacentHTML('beforeEnd', '<div class="item"><div class="item__code">USD</div><div class="item__value">32</div><div class="item__currency">руб.</div></div>');
 
-		items.lastChild.querySelector('.item__code').textContent = data.response.Valute[key].CharCode;
-		items.lastChild.querySelector('.item__value').textContent = data.response.Valute[key].Value;
+			items.lastChild.querySelector('.item__code').textContent = data.response.Valute[key].CharCode;
+			items.lastChild.querySelector('.item__value').textContent = data.response.Valute[key].Value;
+		}
 	}
 }
+
